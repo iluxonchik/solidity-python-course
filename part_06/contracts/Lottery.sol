6f746e624df9777cc80c685cb077b000794735cc
Illya Gerasymchuk <illya@iluxonchik.me>
2022-06-20 01:55:59 +0100

    feat: init basic code skeleton for Lottery app

---
+++ part_06/contracts/Lottery.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.6.6;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
+contract Lottery {
+    // address vs address payable:
+    //     * you can call .transfer() and .send() on a payable address, but not on an address
+    address payable[] public players;
+    uint256 public usdEntryFee;
+    AggregatorV3Interface internal ethUsdPriceFeed;
+
+    constructor(address _priceFeedAddress) public {
+        usdEntryFee = 50 * (10**18);
+        ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+    }
+
+    function enter() public payable {
+        // $50 minimum
+        players.push(msg.sender);
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        //
+    }
+
+    function startLottery() public {}
+}
