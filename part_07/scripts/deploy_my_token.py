a66541ae49130822894a36495b19c3cd15f7b2d9
Illya Gerasymchuk <illya@iluxonchik.me>
2022-07-01 02:05:37 +0300

    refactor: minor refactor

--- part_07/scripts/deploy_my_token.py
+++ part_07/scripts/deploy_my_token.py
@@ -1,10 +1,12 @@
 from scripts.utils import get_account
 from brownie import ILYToken
 
+from web3 import Web3
+
 
 def deploy_my_token():
     account = get_account()
-    ily_token = ILYToken.deploy(69, {"from": account})
+    ily_token = ILYToken.deploy(Web3.toWei(6900, "ether"), {"from": account})
     print("contract deployed!")
     return ily_token
 
