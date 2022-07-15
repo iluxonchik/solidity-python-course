c17d744e2fcc3c4b4d75b66e2c5b0c6c48e2dfdc
Illya Gerasymchuk <illya@iluxonchik.me>
2022-07-15 01:59:14 +0100

    initialize code for NFT chapter

---
+++ part_09/contracts/SimpleCollectible.sol
@@ -0,0 +1,9 @@
+// SPDX-Licence-Idenitfier: MIT
+
+pragma solidity 0.6.6;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract SimpleCollectible is ERC721 {
+    constructor() public ERC721("Dawg", "DOG") {}
+}
