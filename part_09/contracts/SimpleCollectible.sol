f19a955726fc38d4a37861f92fbb7aa4575a8095
Illya Gerasymchuk <illya@iluxonchik.me>
2022-07-19 01:06:42 +0100

    feat: complete simple collectible and add deploy script

--- part_09/contracts/SimpleCollectible.sol
+++ part_09/contracts/SimpleCollectible.sol
@@ -5,5 +5,21 @@ pragma solidity 0.6.6;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract SimpleCollectible is ERC721 {
-    constructor() public ERC721("Dawg", "DOG") {}
+    uint256 public tokenCounter;
+
+    constructor() public ERC721("Dawg", "DOG") {
+        tokenCounter = 0;
+    }
+
+    function createCollectible(string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        // an ERC-721 NFT is just assigining a tokenId to an address
+        uint256 newTokenId = tokenCounter;
+        _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+        tokenCounter += 1;
+        return newTokenId;
+    }
 }
---
+++ part_09/scripts/deploy_and_create.py
@@ -0,0 +1,17 @@
+from lib2to3.pgen2.literals import simple_escapes
+from scripts.utils import get_account, OPENSEA_TESTNET_FORMAT
+from brownie import SimpleCollectible
+
+SIMPLE_TOKEN_URI: str = (
+    "ipfs://Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
+)
+
+
+def main():
+    account = get_account()
+    simple_collectible = SimpleCollectible.deploy({"from": account})
+    tx = simple_collectible.createCollectible(SIMPLE_TOKEN_URI, {"from": account})
+    tx.wait(1)
+    print(
+        f"Your NFT is now available at: {OPENSEA_TESTNET_FORMAT.format(simple_collectible.address, simple_collectible.tokenCounter() - 1)}"
+    )
